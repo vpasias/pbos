0fdbf4ea24fbffc8739ececfdb7df80a4bb20b38
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-13 10:44:34 +0200

    Update vm_deployment-debian.sh

--- vm_deployment-debian.sh
+++ vm_deployment-debian.sh
@@ -141,7 +141,7 @@ for i in {1..6}; do virsh shutdown n$i; done && sleep 10 && virsh list --all && 
 
 sleep 30
 
-for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" ubuntu@n$i "sudo apt update -y"; done
+for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" debian@n$i "sudo apt update -y"; done
 
 for i in {1..6}; do qemu-img create -f qcow2 vbdnode1$i 120G; done
 for i in {1..6}; do qemu-img create -f qcow2 vbdnode2$i 120G; done
