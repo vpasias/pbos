2912e3b7daa11b71b71dfab0a6f5dc6e1e1b4a54
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-15 13:22:20 +0200

    Update vm_deployment-ubuntu.sh

--- vm_deployment-ubuntu.sh
+++ vm_deployment-ubuntu.sh
@@ -125,6 +125,10 @@ for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" ubuntu@n$i "cat << EOF | s
 TimeoutStartSec=15
 EOF"; done
 
+for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" ubuntu@n$i 'sudo sh -c "echo 'Defaults:ubuntu !requiretty' > /etc/sudoers.d/ubuntu"'; done
+for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" ubuntu@n$i 'sudo sh -c "echo '%ubuntu ALL=(ALL) ALL' >> /etc/sudoers.d/ubuntu"'; done
+for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" ubuntu@n$i "sudo chmod 440 /etc/sudoers.d/ubuntu"; done
+
 for i in {1..6}; do virsh shutdown n$i; done && sleep 10 && virsh list --all && for i in {1..6}; do virsh start n$i; done && sleep 10 && virsh list --all
 
 sleep 30
