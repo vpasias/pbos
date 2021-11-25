e65a309ecd9c5f105807dc8b271be1a102bce9a4
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-25 13:47:50 +0200

    Create vm_config.sh

---
+++ rocky/vm_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+
+cat /etc/os-release && uname -a
+dnf -y install cloud-utils-growpart curl epel-release python3 bind-utils
+dnf -y install openssh-server cloud-init sshpass
+dnf install -y net-tools wget curl bash-completion iperf3 mtr traceroute netcat socat python3-simplejson xfsprogs jq virtualenv redhat-lsb-core
+
+mkdir -p /etc/motd.d
+
+cat << EOF | tee /etc/motd.d/01-custom
+****************************WARNING****************************************
+UNAUTHORISED ACCESS IS PROHIBITED. VIOLATORS WILL BE PROSECUTED.
+***************************************************************************
+EOF
+
+chmod +x /etc/motd.d/01-custom
+
+cat << EOF | tee /etc/modprobe.d/kvm.conf
+options kvm_intel nested=1
+EOF
+
+modprobe -r kvm_intel && modprobe -a kvm_intel
+
+mkdir -p /etc/systemd/system/networking.service.d
+
+cat << EOF | tee /etc/systemd/system/networking.service.d/reduce-timeout.conf
+[Service]
+TimeoutStartSec=15
+EOF
+
+modprobe -v xfs && grep xfs /proc/filesystems && modinfo xfs
+
+groupadd rabbitmq
