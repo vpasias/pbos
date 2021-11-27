a80958153776b63070aa304cc3bf90c2d864441a
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-27 19:20:33 +0200

    Update vm_networks.sh

--- vlan/vm_networks.sh
+++ vlan/vm_networks.sh
@@ -18,11 +18,11 @@ cat > /home/iason/vms/mgmt.xml <<EOF
 </network>
 EOF
 
-cat > /home/iason/vms/admin.xml <<EOF
+cat > /home/iason/vms/overlay.xml <<EOF
 <network>
-  <name>admin</name>
+  <name>overlay</name>
   <forward mode='nat'/>
-  <bridge name='admin' stp='off' macTableManager="kernel"/>
+  <bridge name='overlay' stp='off' macTableManager="kernel"/>
   <mtu size="9216"/>
   <mac address='52:54:00:8a:8b:cc'/>
   <ip address='192.168.254.1' netmask='255.255.255.0'>
@@ -54,8 +54,8 @@ cat > /home/iason/vms/provider.xml <<EOF
 </network>
 EOF
 
-virsh net-define /home/iason/vms/admin.xml && virsh net-autostart admin && virsh net-start admin
 virsh net-define /home/iason/vms/mgmt.xml && virsh net-autostart mgmt && virsh net-start mgmt
+virsh net-define /home/iason/vms/overlay.xml && virsh net-autostart overlay && virsh net-start overlay
 virsh net-define /home/iason/vms/storage.xml && virsh net-autostart storage && virsh net-start storage
 virsh net-define /home/iason/vms/provider.xml && virsh net-autostart provider && virsh net-start provider
 
