6d3b7e2080b30b28bd2fc1a391cc834e8fdda1fb
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-27 18:26:18 +0200

    Update vm_networks.sh

--- vlan/vm_networks.sh
+++ vlan/vm_networks.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 #
 
-sudo ovs-vsctl add-br mgmt
-sudo ip addr add 192.168.200.1/24 dev mgmt
-sudo ovs-vsctl set int mgmt mtu_request=9216
-sudo ip link set mgmt up
+#sudo ovs-vsctl add-br mgmt
+#sudo ip addr add 192.168.200.1/24 dev mgmt
+#sudo ovs-vsctl set int mgmt mtu_request=9216
+#sudo ip link set mgmt up
+
+cat > /home/iason/vms/mgmt.xml <<EOF
+<network>
+  <name>mgmt</name>
+  <forward mode='nat'/>
+  <bridge name='mgmt' stp='off' macTableManager="kernel"/>
+  <mtu size="9216"/>
+  <mac address='52:54:00:8a:8b:ca'/>
+  <ip address='192.168.200.1' netmask='255.255.255.0'>
+  </ip>
+</network>
+EOF
 
 cat > /home/iason/vms/admin.xml <<EOF
 <network>
@@ -43,6 +55,7 @@ cat > /home/iason/vms/provider.xml <<EOF
 EOF
 
 virsh net-define /home/iason/vms/admin.xml && virsh net-autostart admin && virsh net-start admin
+virsh net-define /home/iason/vms/mgmt.xml && virsh net-autostart mgmt && virsh net-start mgmt
 virsh net-define /home/iason/vms/storage.xml && virsh net-autostart storage && virsh net-start storage
 virsh net-define /home/iason/vms/provider.xml && virsh net-autostart provider && virsh net-start provider
 
