4805a8b82b08522951f92eaab677e4f672df3e73
vpasias <52623837+vpasias@users.noreply.github.com>
2021-11-27 15:02:31 +0200

    Update deployment.sh

--- vlan/deployment.sh
+++ vlan/deployment.sh
@@ -19,5 +19,134 @@ robot -d output setup_rocky.robot
 sleep 10
 for i in {1..6}; do ssh -o "StrictHostKeyChecking=no" rocky@node-$i "uname -a"; done
 for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-$i "hostnamectl set-hostname node-$i --static"; done
+for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-$i "modprobe 8021q"; done
+for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-$i "lsmod | grep 8021q"; done
+for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-$i "echo 8021q >> /etc/modules-load.d/8021q.conf"; done
+
+# overlay network configuration
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-1 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.201
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-2 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.202
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-3 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.203
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-4 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.204
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-5 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.205
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-6 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.10
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.10
+BOOTPROTO=static
+IPADDR=192.168.210.206
+NETMASK=255.255.255.0
+EOF"
+
+# rados GW network configuration
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-1 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.201
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-2 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.202
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-3 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.203
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-4 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.204
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-5 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.205
+NETMASK=255.255.255.0
+EOF"
+
+sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-6 "cat << EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-eth1.20
+ONBOOT=yes
+TYPE=Ethernet
+VLAN=yes
+DEVICE=eth1.20
+BOOTPROTO=static
+IPADDR=192.168.220.206
+NETMASK=255.255.255.0
+EOF"
+
+for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" root@node-$i "systemctl restart network"; done
+
 for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" rocky@node-$i "ip a"; done
+
 for i in {1..6}; do sshpass -p gprm8350 ssh -o "StrictHostKeyChecking=no" rocky@node-$i "lsblk && uname -a"; done
